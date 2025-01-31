7c5cc84136ecc043e9d86b19d3d18fe7920d9974
vertiKarl <vertikarl@proton.me>
2025-01-31 19:16:06 +0100

    s

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -2,10 +2,13 @@
 
 const { npm_execpath } = process.env;
 
+console.log("Running prepack...");
+
 // prepack is executed by user's package manager when they install from git
 // So cannot assume yarn
 
 if (process.env.TS_NODE_SKIP_PREPACK == null) {
+  console.log("Prepacking");
   const crossSpawn = await import("cross-spawn");
   const result = crossSpawn.sync(npm_execpath, ["run", "prepack-worker"], {
     stdio: "inherit",
